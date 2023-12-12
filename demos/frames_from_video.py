5f446c6561d888a951b6bd6653165e103cf94a88
k00ndra <ondra.kodejs@gmail.com>
2023-12-12 16:37:37 +0100

    merged datasets

--- demos/frames_from_video.py
+++ demos/frames_from_video.py
@@ -12,20 +12,24 @@ def extract_frames(video_path, output_folder):
     # Get video properties
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
+    avail_samples = os.listdir(output_folder)
+    max_id = 0
+    for filename in avail_samples:
+        file_id = int(filename.split('.')[0][6:])
+        max_id = max(max_id, file_id)
+    print(max_id + 1)
 
     # Loop through each frame and save it as an image
     for i in range(frame_count):
         ret, frame = cap.read()
         if not ret:
             break
-
         # Save the frame as an image
-        frame_name = f"frame_{i + 1 + 714}.jpg"
-        frame_path = os.path.join(output_folder, frame_name)
-        cv2.imwrite(frame_path, frame)
-
-    # Release the video capture object
-    cap.release()
+        if i % 80 == 0:
+            frame_name = f"frame_{max_id + 1}.jpg"
+            max_id += 1
+            frame_path = os.path.join(output_folder, frame_name)
+            cv2.imwrite(frame_path, frame)
 
 if __name__ == "__main__":
     # Set the path to the video file and the output folder
