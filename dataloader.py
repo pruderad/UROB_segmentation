7e70aa7b1646b9ec1a4996036df710eb0a411ff4
k00ndra <ondra.kodejs@gmail.com>
2023-12-16 03:34:00 +0100

    update

--- dataloader.py
+++ dataloader.py
@@ -3,6 +3,7 @@ import pickle
 import numpy as np
 import torch
 from PIL import ImageOps, Image
+from torchvision import transforms
 class UROBDataset(Dataset):
 
     def __init__(self, filenames_file: str, target_img_shape: list,  label_mapping: dict = None) -> None:
@@ -18,6 +19,8 @@ class UROBDataset(Dataset):
 
         self.target_shape = target_img_shape
 
+        self.transform = transforms.Compose([transforms.Resize(512, interpolation=Image.BICUBIC, antialias=True)])
+
     def __len__(self):
         return len(self.filenames)
 
@@ -27,24 +30,37 @@ class UROBDataset(Dataset):
             img = file['X']
             sem_seg = file['y']
 
-            print(img.shape, sem_seg.shape)
 
         labels = np.zeros_like(sem_seg, dtype=float)
         for label_mapping_key in self.label_mapping.keys():
             mask = sem_seg == label_mapping_key
             labels[mask] = self.label_mapping[label_mapping_key]
-
-        img = 2 * img / 255 - 1
-
-        # resize the image
-        img = self.padd(img, labels).transpose((2,1,0))
         
+        img, labels = self.fix_sizes(img_orig=img, labels_orig=labels)
+        img = 2 * (img / 255) - 1 
+        img = np.transpose(img, (2, 0, 1))
 
-        # TODO() delete
-        labels = np.ones(self.target_shape[::-1] + [1])
-        #print(img.shape, labels.shape)
         return img, labels
 
+
+    def fix_sizes(self, img_orig: np.ndarray, labels_orig: np.ndarray):
+
+        #! assuming the labels are resized correctly
+        img_pil = Image.fromarray(img_orig)
+        transformed_img = np.asarray(self.transform(img_pil))
+
+        img = np.zeros((*self.target_shape, 3), dtype=transformed_img.dtype)
+        labels = np.zeros(self.target_shape, dtype=labels_orig.dtype) # TODO() add ignore labels
+        start_idx = (self.target_shape[1] - transformed_img.shape[1]) // 2 
+        assert start_idx >= 0
+
+        img[:, start_idx: start_idx + transformed_img.shape[1], :] = transformed_img
+        labels[:, start_idx: start_idx + transformed_img.shape[1]] = labels_orig
+
+        #print(transformed_img.shape, self.target_shape)
+        #print(img.shape, labels.shape)
+
+        return img, labels
 
 
 
