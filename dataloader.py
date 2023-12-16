622de9318993957726519236ade935d05a9a4d53
k00ndra <ondra.kodejs@gmail.com>
2023-12-16 02:20:33 +0100

    update

--- dataloader.py
+++ dataloader.py
@@ -2,19 +2,21 @@ from torch.utils.data import DataLoader, Dataset
 import pickle   
 import numpy as np
 import torch
-
+from PIL import ImageOps, Image
 class UROBDataset(Dataset):
 
-    def __init__(self, filenames_file: str, label_mapping: dict = None) -> None:
+    def __init__(self, filenames_file: str, target_img_shape: list,  label_mapping: dict = None) -> None:
         with open(filenames_file, 'rb') as file:
             self.filenames = pickle.load(file)
 
         if label_mapping is not None:
             self.label_mapping = label_mapping
         else:
-            label_mapping = {
+            self.label_mapping = {
                 2 : 1 # car
             }
+
+        self.target_shape = target_img_shape
 
     def __len__(self):
         return len(self.filenames)
@@ -25,20 +27,49 @@ class UROBDataset(Dataset):
             img = file['X']
             sem_seg = file['y']
 
+            print(img.shape, sem_seg.shape)
+
         labels = np.zeros_like(sem_seg, dtype=float)
         for label_mapping_key in self.label_mapping.keys():
             mask = sem_seg == label_mapping_key
             labels[mask] = self.label_mapping[label_mapping_key]
 
-        labels = torch.from_numpy(labels)
-        img = torch.from_numpy(img).permute(3, 1, 2)
         img = 2 * img / 255 - 1
 
-        return img, labels
-
+        # resize the image
+        img = self.padd(img, labels).transpose((2,1,0))
         
 
+        # TODO() delete
+        labels = np.ones(self.target_shape[::-1] + [1])
+        #print(img.shape, labels.shape)
+        return img, labels
 
+
+    def padd(self, image: np.ndarray, labels: np.ndarray, ignore_label = 30):
+        
+
+        h, w, c = image.shape
+        
+        if w > h:
+            image = np.transpose(image, (1, 0, 2))
+            labels = labels.T
+            h, w, c = image.shape
+
+        result_labels = np.full(self.target_shape + [1], ignore_label, dtype=image.dtype)
+        result = np.zeros(self.target_shape + [c],dtype=image.dtype)
+        # Calculate the starting position to place the image in the center
+        start_h = (self.target_shape[0] - h) // 2
+        start_w = (self.target_shape[1] - w) // 2
+
+        # Copy the image to the center of the result array
+        print(image.shape, labels.shape)
+        print(labels.reshape((h,w,1)).shape, [h,w,c])
+        result[start_h:start_h + h, start_w:start_w + w, :] = image
+        result_labels[start_h:start_h + h, start_w:start_w + w, :] = labels.reshape((h,w,1))
+
+        return result
+            
 
 
 
--- parse_remote_dataset.py
+++ parse_remote_dataset.py
@@ -3,26 +3,35 @@ import os
 import sys
 from PIL import Image
 from tqdm import tqdm
+import re
 
 IMG_FOLDER = 'rgb'
 SEG_FOLDER = 'seg'
 VIS_FOLDER = 'vis'
 
 
-def process_sample(mount_path: str, output_path: str, sample_id: int):
-    img_sample_name = f'frame_{sample_id}.jpg'
-    seg_sample_name = f'frame_{sample_id}.npy'
-    out_sample_name = f'sample_{sample_id}.npz'
-
-    img_path = os.path.join(mount_path, IMG_FOLDER, img_sample_name)
-    seg_path = os.path.join(mount_path, SEG_FOLDER, seg_sample_name)
-    vis_path = os.path.join(mount_path, VIS_FOLDER, img_sample_name)
-    out_path = os.path.join(output_path, out_sample_name)
+def process_sample(jpg_sample_name: str, output_folder: str, source_folder: str):
     
+    pattern = r'frame_(\d+)\.jpg'
+    match = re.match(pattern, jpg_sample_name)
+    if match:
+        id_number = int(match.group(1))
+    else:
+        print('n id: ', jpg_sample_name)
+        pass
+
+    jpg_path = os.path.join(source_folder, IMG_FOLDER, jpg_sample_name)
+    vis_path = os.path.join(source_folder, VIS_FOLDER, f'frame_{id_number}.jpg')
+    seg_path = os.path.join(source_folder, SEG_FOLDER, f'frame_{id_number}.npy')
+
+    out_path = os.path.join(output_folder, f'sample_{id_number}.npz')
+
     try:
-        img = np.array(Image.open(img_path))
+        img = np.array(Image.open(jpg_path))
         img_vis = np.array(Image.open(vis_path))
         seg = np.load(seg_path)
+
+        print(f'{id_number}: {img.shape} / {img_vis.shape} / {seg.shape}')
         np.savez(out_path, X=img, y=seg, vis=img_vis)
     except Exception as e:
         print(e)
@@ -35,8 +44,8 @@ if __name__ == '__main__':
     pth = os.path.join(mount_path, 'vis/')
     files = os.listdir(pth)
     pbar = tqdm(total=len(files))
-    for i, _ in enumerate(files):
-        process_sample(mount_path=mount_path, output_path=output_folder, sample_id=i)
+    for filename in os.listdir(os.path.join(mount_path, IMG_FOLDER)):
+        process_sample(jpg_sample_name=filename, output_folder=output_folder, source_folder=mount_path)
         pbar.update(1)
     pbar.close()
     print('done')
