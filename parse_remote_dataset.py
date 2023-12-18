9e394d51880cddfc5168d59af3e77797d69eadd9
k00ndra <ondra.kodejs@gmail.com>
2023-12-18 17:40:06 +0100

    fixed PrvniaDruha dataset edgecase

--- parse_remote_dataset.py
+++ parse_remote_dataset.py
@@ -9,20 +9,30 @@ IMG_FOLDER = 'rgb'
 SEG_FOLDER = 'seg'
 VIS_FOLDER = 'vis'
 
+special_label_mapping = {
+    2 : 40, # car,
+    3: 40,
+    7: 40
+}
+
 
 def process_sample(jpg_sample_name: str, output_folder: str, source_folder: str):
     
-    pattern = r'frame_(\d+)\.jpg'
-    match = re.match(pattern, jpg_sample_name)
-    if match:
-        id_number = int(match.group(1))
-    else:
-        print('n id: ', jpg_sample_name)
-        pass
+
+    if jpg_sample_name.endswith('jpg') or jpg_sample_name.endswith('.png'):
+        id_number_str = jpg_sample_name.split('.')[0][6:]
+        if id_number_str[-1] == 'a':
+            id_number = int(id_number_str[:-1]) # a correction
+        else:
+            id_number = int(id_number_str)
 
     jpg_path = os.path.join(source_folder, IMG_FOLDER, jpg_sample_name)
-    vis_path = os.path.join(source_folder, VIS_FOLDER, f'frame_{id_number}.jpg')
-    seg_path = os.path.join(source_folder, SEG_FOLDER, f'frame_{id_number}.npy')
+    if jpg_sample_name.endswith('jpg'):
+        vis_path = os.path.join(source_folder, VIS_FOLDER, f'frame_{id_number_str}.jpg')
+    # png correction
+    if jpg_sample_name.endswith('.png'):
+        vis_path = os.path.join(source_folder, VIS_FOLDER, f'frame_{id_number_str}.png')
+    seg_path = os.path.join(source_folder, SEG_FOLDER, f'frame_{id_number_str}.npy')
 
     out_path = os.path.join(output_folder, f'sample_{id_number}.npz')
 
@@ -31,7 +41,11 @@ def process_sample(jpg_sample_name: str, output_folder: str, source_folder: str)
         img_vis = np.array(Image.open(vis_path))
         seg = np.load(seg_path)
 
-        print(f'{id_number}: {img.shape} / {img_vis.shape} / {seg.shape}')
+        # modify the labels
+        for label_orig in special_label_mapping.keys():
+            label_mask = seg == label_orig
+            seg[label_mask] = special_label_mapping[label_orig]
+        #print(f'{id_number}: {img.shape} / {img_vis.shape} / {seg.shape}')
         np.savez(out_path, X=img, y=seg, vis=img_vis)
     except Exception as e:
         print(e)
